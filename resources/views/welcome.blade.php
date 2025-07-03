25ff16ff554ba55835a8ea7cdbd6ad7c6d751e6c
danapalania <dana.p.alania@ciit.edu.ph>
2025-07-03 14:37:47 +0800

    updates na di maayos proceed w caution <3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
+        <link href="/resources/css/app.css" rel="stylesheet">
+        @vite('resources/css/landing.css')
+
         <title>insert blog name here</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -12,7 +14,28 @@
         <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&display=swap" rel="stylesheet">
 
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-
+    <body>
+        <nav class = "navbar" class="shadow-md">
+            <div class = "logo">
+                <i class="ri-cup-line"></i>
+                <a href = "index.html" class = "blog_name">Blog Name</a>
+            </div>
+            <div class = "navbar_content">
+                <ul class = "navbar_items">
+                    <li><a href = "#">HOME</a></li>
+                    <li><a href = "#">ABOUT</a></li>
+                    <li><a href = "#">CONTACT</a></li>
+                    <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
+                    <li class = "dropdown">
+                        <i class="ri-menu-3-line"></i>
+                        <div class = "dropdown_content">
+                            <a href = "#">Account</a>
+                            <a href = "#">My Posts</a>
+                            <a href = "#">Log Out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </body>
 </html>
