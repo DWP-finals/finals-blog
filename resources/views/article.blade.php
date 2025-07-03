cd27c7b5608c3a778202e2d33c373b99ca72bb01
julianasj <juliana.jimeno@ciit.edu.ph>
2025-07-03 15:34:16 +0800

    ermm

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -5,8 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="/resources/css/app.css" rel="stylesheet">
+    @vite('resources/css/article.css')
 </head>
 <body>
-    <h1>slay</h1>
+    <!--========== NAVIGATION BAR ==========-->
+    <nav class = "navbar" class="shadow-md">
+        <div class = "logo">
+            <i class="ri-cup-line"></i>
+            <a href = "index.html" class = "blog_name">Blog Name</a>
+        </div>
+        <div class = "navbar_content">
+            <ul class = "navbar_items">
+                <li><a href = "#">HOME</a></li>
+                <li><a href = "#">ABOUT</a></li>
+                <li><a href = "#">CONTACT</a></li>
+                <button class="btn">Sign Up</button>
+                <li class = "dropdown">
+                    <i class="ri-menu-3-line"></i>
+                    <div class = "dropdown_content">
+                        <a href = "#">Account</a>
+                        <a href = "#">My Posts</a>
+                        <a href = "#">Log Out</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!--========== ARTICLE IMAGE CONTAINER ==========-->
+    <div class = "article-img">
+        <img class = "imgcontainer" src = "img.jpg">
+    </div>
+
 </body>
 </html>
