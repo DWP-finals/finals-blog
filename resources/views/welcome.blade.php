6dd2973a12297dde1952c05e6f6c3e5cdf8a74aa
danapalania <dana.p.alania@ciit.edu.ph>
2025-07-09 11:02:05 +0800

    ermermerm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,5 +38,22 @@
                 </ul>
             </div>
         </nav>
+        <div class="sec1">
+            <div class="slider">
+                <div class="slides">
+                    <div id="slide-1">1</div>
+                    <div id="slide-2">2</div>
+                    <div id="slide-3">3</div>
+                    <div id="slide-4">4</div>
+                    <div id="slide-5">5</div>
+                </div>
+                <br>
+                <a href="#slide-1"></a>
+                <a href="#slide-2"></a>
+                <a href="#slide-3"></a>
+                <a href="#slide-4"></a>
+                <a href="#slide-5"></a>
+            </div>
+        </div>
     </body>
 </html>
